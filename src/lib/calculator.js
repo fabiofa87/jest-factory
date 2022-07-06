a47440199252a6e33a3bc380a0f7d72de0f87f43
fabiofa87 <fabiofadev87@gmail.com>
2022-07-06 19:15:52 -0300

    Corrigido de modules para functions

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -1,10 +1,10 @@
-module.exports.sum = (a, b) => {
+export function sum(a, b) {
   const int1 = parseInt(a, 10);
   const int2 = parseInt(b, 10);
 
   if (Number.isNaN(int1) || Number.isNaN(int2)) {
-    throw new Error("Arguments must be numbers");
+    throw new Error('Arguments must be numbers');
   }
 
   return +a + +b;
-};
+}
--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -5,11 +5,12 @@ const keyValueToString = ([key, value]) => {
   return `${key}=${value}`;
 };
 
-module.exports.queryString = obj =>
-  Object.entries(obj).map(keyValueToString).join('&');
+export function queryString(obj) {
+  return Object.entries(obj).map(keyValueToString).join('&');
+}
 
-module.exports.parseString = string =>
-  Object.fromEntries(
+export function parseString(string) {
+  return Object.fromEntries(
     string.split('&').map(item => {
       let [key, value] = item.split('=');
 
@@ -20,3 +21,4 @@ module.exports.parseString = string =>
       return [key, value];
     }),
   );
+}
